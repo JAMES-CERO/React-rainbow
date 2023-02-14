a5b47695b66bc151e3904ad2705ecf8f0fde53ef
James Aules <james_aules@icloud.com>
2023-02-14 15:50:31 -0500

    function Colorform.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import ColorBlock from './ColorBlock';
+import ColorForm from './ColorForm';
 
 function App() {
-  let colors = ["violtes", "blue", "lightblue", "green", "greenyellow", "yellow", "orange", "red"]
-
+  const addColor = (newColor) => {
+    setColors([...colors, newColor])
+  }
+  let [colors, setColors] = useState(["violet", "blue", "lightblue", "green", "greenyellow", "yellow", "orange", "red"]
+  )
   return (
     <div className="App">
       {colors.map((color, i) => 
         <ColorBlock key={color} color={color} />
       )}
+      <ColorForm addColor={addColor} />
     </div>
   );
 }
